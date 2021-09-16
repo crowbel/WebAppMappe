0118cc0b1c2ad75e619b7373f8a5bdaffd89653c
crowbel <magkrstar@gmail.com>
2021-09-16 12:51:29 +0200

    Intiial Commit - Oppsett av prosjektstruktur.

--- WebAppMappeProsjekt/Startup.cs
+++ WebAppMappeProsjekt/Startup.cs
@@ -23,7 +23,10 @@ namespace WebAppMappeProsjekt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
+            //services.AddDbContect<BestillingContext>(OptionsBuilderConfigurationExtensions =>
+            //OptionsBuilderConfigurationExtensions.UseSqlite("Data Source=Bestilling.db"));
+            //Åpne kommentar når Bestilling.db er opprettet!
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,23 +36,19 @@ namespace WebAppMappeProsjekt
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
