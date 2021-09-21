21c30717eb952d6ad818306b1bc7a2b6fd30345e
crowbel <magkrstar@gmail.com>
2021-09-21 11:56:10 +0200

    Opprettet oppsett for RuteController med planlagt HentAlleRuter()

---
+++ WebAppMappeProsjekt/Controllers/RuteController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMappeProsjekt.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class RuteController : ControllerBase
+    {
+        //private readonly RuteContext _db; // RuteContext ikke merget inn enda.
+
+        /*public RuteController(RuteContext db) //RuteContext ikke merget inn enda.
+        {
+            _db = db;
+        }
+        */
+
+
+        //Metode for HentAlleRuter følger her.
+
+        /*
+         
+        public async Task<List<Rute>> HentAlleRuter()
+        {
+            try
+            {
+                List<Rute> alleRuter = await _db.Ruter.Select(k => new Rute
+                {
+                    Id = r.Id,
+                    FraDestinasjon = r.FraDestinajon,
+                    TilDestinasjon = r.TilDestinasjon,
+                    PrisBarn = r.PrisBarn,
+                    PrisVoksen = r.PrisVoksen
+                }).ToListAsync();
+                
+                return alleRuter;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    
+         */ 
+    }
+}
