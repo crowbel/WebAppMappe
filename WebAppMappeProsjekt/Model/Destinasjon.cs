9c70d88ca77d40cb9d52142abb029367a5bd09f6
crowbel <magkrstar@gmail.com>
2021-09-21 11:12:50 +0200

    Opprettet model for Destinasjon.

--- WebAppMappeProsjekt/Model/Destinasjon.cs
+++ WebAppMappeProsjekt/Model/Destinasjon.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAppMappeProsjekt.Model
 {
-    public class Destinasjon
+    public class Destinasjoner
     {
+        public int Id { get; set; }
+
+        public string Navn { get; set; }
+
+        public string Land { get; set; }
+    }
+
+
+    public class DestinasjonContext : DbContext
+    {
+        public DestinasjonContext(DbContextOptions<DestinasjonContext> options)
+                : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Destinasjon> Destinasjoner { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
     }
 }
