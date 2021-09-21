4e43470ab8a320b690b3ea68f3eacb64e8ec549a
crowbel <magkrstar@gmail.com>
2021-09-21 11:24:32 +0200

    Opprettet Model for Rute og korrigert Model for Destinasjon

--- WebAppMappeProsjekt/Model/Destinasjon.cs
+++ WebAppMappeProsjekt/Model/Destinasjon.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,25 +8,7 @@ namespace WebAppMappeProsjekt.Model
     public class Destinasjoner
     {
         public int Id { get; set; }
-
         public string Navn { get; set; }
-
         public string Land { get; set; }
-    }
-
-
-    public class DestinasjonContext : DbContext
-    {
-        public DestinasjonContext(DbContextOptions<DestinasjonContext> options)
-                : base(options)
-        {
-            Database.EnsureCreated();
-        }
-        public DbSet<Destinasjon> Destinasjoner { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseLazyLoadingProxies();
-        }
     }
 }
--- WebAppMappeProsjekt/Model/Rute.cs
+++ WebAppMappeProsjekt/Model/Rute.cs
@@ -7,5 +7,10 @@ namespace WebAppMappeProsjekt.Models
 {
     public class Rute
     {
+        public int Id { get; set; }
+        public int FraDestinasjon { get; set; }
+        public int TilDestinasjon { get; set; }
+        public int PrisBarn { get; set; }
+        public int PrisVoksen { get; set; }
     }
 }
