33ad82ad970289e98f087a7512b05951fc4e720a
Thea <thealp@altiboxmail.no>
2021-09-23 13:07:23 +0200

    * OrdreController.cs: Laget metoder i ordrecontroller
    * BillettOrdre.cs: Fikset feil
    
    * OrdreContext.cs:

--- WebAppMappeProsjekt/Controllers/OrdreController.cs
+++ WebAppMappeProsjekt/Controllers/OrdreController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAppMappeProsjekt.Model;
 
 namespace WebAppMappeProsjekt.Controllers
@@ -18,12 +20,50 @@ namespace WebAppMappeProsjekt.Controllers
 
         public async Task<bool> LagreOrdre(BillettOrdre innOrdre)
         {
-            return true;
+            try
+            {
+                var nyOrdre = new Ordrer();
+                nyOrdre.antallBarn = innOrdre.AntallBarn;
+                nyOrdre.antallVoksne = innOrdre.AntallVoksen;
+                nyOrdre.refPerson = innOrdre.RefPers;
+                nyOrdre.Avgang = innOrdre.AvgangNr;
+                nyOrdre.RuteNr = innOrdre.RuteNr;
+
+                _db.Ordrer.Add(nyOrdre);
+                await _db.SaveChangesAsync();
+                return true;
+
+                
+            }
+            catch
+            {
+                return false;
+            }
         }
+
 
         public async Task<List<BillettOrdre>> HentAlle()
         {
-            return null;
+            try
+            {
+                List<BillettOrdre> alleOrdrer = await _db.Ordrer.Select(b => new BillettOrdre
+                {
+                    Id = b.Id,
+                    AntallBarn = b.antallBarn,
+                    AntallVoksen= b.antallVoksne,
+                    RefPers = b.refPerson,
+                    AvgangNr = b.Avgang,
+                    RuteNr = b.RuteNr
+
+                 
+                }).ToListAsync();
+
+                return alleOrdrer;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
