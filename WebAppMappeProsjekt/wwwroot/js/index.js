da1216c91d0c5861518e420005ec7d737879c1ac
Thea <thealp@altiboxmail.no>
2021-09-23 14:40:00 +0200

    * WebAppMappeProsjekt.csproj:
    * index.js: index.js: lagreBestilling

--- WebAppMappeProsjekt/wwwroot/js/index.js
+++ WebAppMappeProsjekt/wwwroot/js/index.js
@@ -68,11 +68,23 @@ function bestillingsVindu() {
 //Funksjon som prosseserer valgene fra bestillingsvinduet og lagrer dem til databasen.
 function lagreBestilling() {
 
+
     //Sjekker at informasjonen oppgitt i bestillingsvinduet er gyldig f.eks Simple RegEx
 
     //Tar informasjon ifra bestillingsvinduet og lagrer dette til bestillingsdatabasen.
+    const ordre = {
+        antallBarn: $("#antallBarn"),
+        antallVoksne: $("#antallVoksne"),
+        refPerson: $("#refPerson"),
+        avgang: $("#avgang"),
+        ruteNr: $("#ruteNr")
+    }
 
     //Kaller på en Funksjon hentBestillinger() som henter alle bestillinger i databasen.
+    const url = "Ordre/LagreOrdre";
+    $.post(url, ordre, function () {
+        hentBestillinger();
+    });
 
 }
 
