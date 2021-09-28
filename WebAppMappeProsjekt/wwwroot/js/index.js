c6f355d68ab5a4fb2ea2128edd01d9c6fc6eb30d
bruker <s344066@oslomet.no>
2021-09-28 11:52:39 +0200

    lagt inn funksjon for bestilling

--- WebAppMappeProsjekt/wwwroot/js/index.js
+++ WebAppMappeProsjekt/wwwroot/js/index.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
 
     hentAlleDestinasjoner();
+    bestillingsVindu();
 });
 
 function hentAlleDestinasjoner() {
@@ -73,7 +74,37 @@ function ruteVelger() {
 
 
 //Funksjon som prosesserer valgene fra/til og går videre til bestillingsvindu.
+
 function bestillingsVindu() {
+
+    let ut = "<div class='container' style='width: 50%'>" +
+        "<h1>Info</h1>" +
+        "<form class='form'>" +
+        "<div class='form-group'>" +
+        "<label>Antall Barn</label></br>" +
+        "<input type='number' class='form-control' id='antallBarn'/>" +
+        "<span id='feilAntallBarn' style='color: red'></span>" +
+        "</div>" +
+        "<div class='form-group'>" +
+        "<label>Antall Voksne</label></br>" +
+        "<input type='number' class='form-control' id='antallVoksen'/>" +
+        "<span id='feilAntallVoksne' style='color: red'></span>" +
+        "</div>" +
+        "<div class='form-group'>" +
+        "<label>Fornavn og Etternavn</label></br>" +
+        "<input type='text' class='form-control' id='refPers'/>" +
+        "<span id='feilRefPerson' style='color: red'></span>" +
+        "</div>" +
+        "<input type='hidden' id='avgangNr'/>" +
+        "<input type='hidden' id='ruteNr'/>" +
+        "<div class='form-group'>" +
+        "<input type='button' id='lagre' value='Neste' onclick='lagreBestilling()' class='btn btn-primary'/>" +
+        "</div>" +
+        "</form>" +
+        "</div>";
+
+
+    $("#outputOmråde").html(ut);
 
     //Lagrer fra og til info midlertidig
 
@@ -84,23 +115,24 @@ function bestillingsVindu() {
 
 
 //Funksjon som prosseserer valgene fra bestillingsvinduet og lagrer dem til databasen.
+
 function lagreBestilling() {
 
 
     //Sjekker at informasjonen oppgitt i bestillingsvinduet er gyldig f.eks Simple RegEx
 
     //Tar informasjon ifra bestillingsvinduet og lagrer dette til bestillingsdatabasen.
-    const ordre = {
-        antallBarn: $("#antallBarn"),
-        antallVoksne: $("#antallVoksne"),
-        refPerson: $("#refPerson"),
-        avgang: $("#avgang"),
-        ruteNr: $("#ruteNr")
+    const order = {
+        antallBarn: $("#antallBarn").val(),
+        antallVoksen: $("#antallVoksen").val(),
+        refPers: $("#refPers").val(),
+        avgangNr: $("#avgangNr").val(),
+        ruteNr: $("#ruteNr").val()
     }
 
     //Kaller på en Funksjon hentBestillinger() som henter alle bestillinger i databasen.
     const url = "Ordre/LagreOrdre";
-    $.post(url, ordre, function () {
+    $.post(url, order, function () {
         hentBestillinger();
     });
 
@@ -113,27 +145,26 @@ function hentBestillinger() {
     $.get("Ordre/HentAlle", function (ordre) {
         formaterOrdre(ordre);
     })
-    .fail(function () {
-        $("#feil").html("Feil på server - prøv igjen senere");
-    });
+        .fail(function () {
+            $("#feil").html("Feil på server - prøv igjen senere");
+        });
 }
 
 function formaterOrdre(ordre) {
     let ut = "<table class='table table-striped'>" +
         "<tr>" +
-        "<th>Antall Voksne</th><th>Antall Barn</th><th>RefPerson</th><th>Avgang</th><th>Rute Nr</th>" +
+        "<th>Antall Barn</th><th>Antall Voksne</th><th>Navn</th><th>Avgang</th><th>Rute Nr</th>" +
         "</tr>";
-
     for (let order of ordre) {
         ut += "<tr>" +
-            "<td>" + order.antallVoksne + "</td>" +
             "<td>" + order.antallBarn + "</td>" +
-            "<td>" + order.refPerson + "</td>" +
-            "<td>" + order.avgang + "</td>" +
+            "<td>" + order.antallVoksen + "</td>" +
+            "<td>" + order.refPers + "</td>" +
+            "<td>" + order.avgangNr + "</td>" +
             "<td>" + order.ruteNr + "</td>" +
             "</tr>";
     }
 
-    ut += "</table>"
-    $("#outputOmråde").html(ut);   
+    ut += "</table>";
+    $("#outputOmråde").html(ut);
 }
