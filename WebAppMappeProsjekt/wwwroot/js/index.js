cc8ae853ab128bf99c9738dd228e228a8467b22e
Louise P. Myhre <louise-pm@hotmail.no>
2021-09-29 13:24:40 +0200

    rute id sent til bestilling.html

--- WebAppMappeProsjekt/wwwroot/js/index.js
+++ WebAppMappeProsjekt/wwwroot/js/index.js
@@ -1,7 +1,5 @@
 ﻿$(function () {
-
     hentAlleDestinasjoner();
-    knapp();
 });
 
 function hentAlleDestinasjoner() {
@@ -18,17 +16,7 @@ function visDestinasjoner(destinasjoner) {
         ut += "<option value='"+dest.id+"'>" + dest.sted + "</option>";
     }
     
-    $("#ruteVelger").html(ut);
-   
-}
-
-function knapp() {
-    ut = "<button class='btn btn-default' onclick='tilBestilling()'>Videre</button>";
-    $("#knapp").html(ut);
-}
-
-function tilBestilling() {
-    window.location.href = "bestilling.html";
+    $("#ruteVelger").html(ut); 
 }
 
 
@@ -71,99 +59,15 @@ function hentAvganger() {
         formaterAvganger(avganger);
     });
     $("#ruteOutput").html(id);
+
+    knapp(id)
 }
 function formaterAvganger(avganger) {
 
 }
 
-/*function knapp() {
-    ut = "<a class='btn btn-default href='bestilling.html?id=+' "
-}*/
-//Funksjon som prosesserer valgene fra/til og går videre til bestillingsvindu.
-
-/*function bestillingsVindu() {
-
-    let ut = "<div class='container' style='width: 50%'>" +
-        "<h1>Info</h1>" +
-        "<form class='form'>" +
-        "<div class='form-group'>" +
-        "<label>Antall Barn</label></br>" +
-        "<input type='number' class='form-control' id='antallBarn'/>" +
-        "<span id='feilAntallBarn' style='color: red'></span>" +
-        "</div>" +
-        "<div class='form-group'>" +
-        "<label>Antall Voksne</label></br>" +
-        "<input type='number' class='form-control' id='antallVoksen'/>" +
-        "<span id='feilAntallVoksne' style='color: red'></span>" +
-        "</div>" +
-        "<div class='form-group'>" +
-        "<label>Fornavn og Etternavn</label></br>" +
-        "<input type='text' class='form-control' id='refPers'/>" +
-        "<span id='feilRefPerson' style='color: red'></span>" +
-        "</div>" +
-        "<input type='hidden' id='avgangNr'/>" +
-        "<input type='hidden' id='ruteNr'/>" +
-        "<div class='form-group'>" +
-        "<input type='button' id='lagre' value='Neste' onclick='lagreBestilling()' class='btn btn-primary'/>" +
-        "</div>" +
-        "</form>" +
-        "</div>";
-
-
-    $("#outputOmråde").html(ut);
-
-    
+function knapp(id) {
+    ut = "<a class='btn btn-default' href='bestilling.html?id=" + id + "'>Videre</a>";
+    $("#knapp").html(ut);
 }
 
-
-
-function lagreBestilling() {
-
-
-    
-    const order = {
-        antallBarn: $("#antallBarn").val(),
-        antallVoksen: $("#antallVoksen").val(),
-        refPers: $("#refPers").val(),
-        avgangNr: $("#avgangNr").val(),
-        ruteNr: $("#ruteNr").val()
-    }
-
-   
-    const url = "Ordre/LagreOrdre";
-    $.post(url, order, function () {
-        hentBestillinger();
-    });
-
-}
-
-
-
-function hentBestillinger() {
-    
-    $.get("Ordre/HentAlle", function (ordre) {
-        formaterOrdre(ordre);
-    })
-        .fail(function () {
-            $("#feil").html("Feil på server - prøv igjen senere");
-        });
-}
-
-function formaterOrdre(ordre) {
-    let ut = "<table class='table table-striped'>" +
-        "<tr>" +
-        "<th>Antall Barn</th><th>Antall Voksne</th><th>Navn</th><th>Avgang</th><th>Rute Nr</th>" +
-        "</tr>";
-    for (let order of ordre) {
-        ut += "<tr>" +
-            "<td>" + order.antallBarn + "</td>" +
-            "<td>" + order.antallVoksen + "</td>" +
-            "<td>" + order.refPers + "</td>" +
-            "<td>" + order.avgangNr + "</td>" +
-            "<td>" + order.ruteNr + "</td>" +
-            "</tr>";
-    }
-
-    ut += "</table>";
-    $("#outputOmråde").html(ut);
-}*/
