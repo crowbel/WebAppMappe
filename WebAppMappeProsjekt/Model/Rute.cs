bb66b9a11ce49ec4ad64d645e8fede60b6d5129e
Nikolai Kopperudmoen <nkopperudmoen@gmail.com>
2021-10-05 14:18:25 +0200

    Lagt til riktige attributter i modellene

--- WebAppMappeProsjekt/Model/Rute.cs
+++ WebAppMappeProsjekt/Model/Rute.cs
@@ -9,8 +9,8 @@ namespace WebAppMappeProsjekt.Models
     public class Rute
     {
         public int Id { get; set; }
-        virtual public Destinasjoner FraDestinasjon { get; set; }
-        virtual public Destinasjoner TilDestinasjon { get; set; }
+        public string FraDestinasjon { get; set; }
+        public string TilDestinasjon { get; set; }
         public int PrisBarn { get; set; }
         public int PrisVoksen { get; set; }
     }
